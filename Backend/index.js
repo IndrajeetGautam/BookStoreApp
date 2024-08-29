f9a7fabb5ff4470cb902dbdcb33f608314cb5243
Indrajeet Singh Gautam <indrajeet45900@gmail.com>
2024-08-30 01:50:40 +0530

    last commit

--- Backend/index.js
+++ Backend/index.js
@@ -6,6 +6,7 @@ import cors from "cors";
 import bookRoute from "./route/book.route.js";
 import userRoute from "./route/user.route.js";
 
+
 const app = express();
 
 app.use(cors());
@@ -30,6 +31,11 @@ try {
 // defining routes
 app.use("/book", bookRoute);
 app.use("/user", userRoute);
+//Deployment
+
+
+
+
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
